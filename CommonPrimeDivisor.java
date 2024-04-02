7b5f283ef809ce0fa644c2b9b411d709ae1cf4d2
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-04-01 18:22:08 -0600

    re-factor gcd method to reduce one calculation

--- CommonPrimeDivisor.java
+++ CommonPrimeDivisor.java
@@ -89,7 +89,7 @@ public class CommonPrimeDivisor {
     }
 
     public static int gcd(int a, int b) {
-        if (a % b == 0) return b;
+        if (b == 0) return a;
         return gcd(b, a % b);
     }
 }
